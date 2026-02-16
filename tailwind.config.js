968f57454bbb63c09e992b5df2e87b1769ca0a15
leyrecilla <leyre.tf@gmail.com>
2026-02-16 06:56:54 +0100

    Add Tailwind CSS configuration with orange colors

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark-orange': '#FF8C00',
+        'light-orange': '#FFA500',
+      },
+    },
+  },
+  plugins: [],
+};
